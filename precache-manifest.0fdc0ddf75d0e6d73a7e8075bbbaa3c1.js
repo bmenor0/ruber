8737d39060124617e5d6f87d05a2b2667f2a309f
bmenor0 <58092673+bmenor0@users.noreply.github.com>
2019-12-05 11:53:14 -0800

    Updates

---
+++ precache-manifest.0fdc0ddf75d0e6d73a7e8075bbbaa3c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54e592da611f714458811d0c13ed97c6",
+    "url": "/ruber/index.html"
+  },
+  {
+    "revision": "007599ea65914caf9890",
+    "url": "/ruber/static/css/main.ff9c5490.chunk.css"
+  },
+  {
+    "revision": "b3f6f993f7e913af15dd",
+    "url": "/ruber/static/js/2.14e8dcaa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ruber/static/js/2.14e8dcaa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "007599ea65914caf9890",
+    "url": "/ruber/static/js/main.81ba3740.chunk.js"
+  },
+  {
+    "revision": "8d820b70fddc34b59784",
+    "url": "/ruber/static/js/runtime-main.b040701f.js"
+  },
+  {
+    "revision": "b6f3e47048f1bf64597846bd4bafce15",
+    "url": "/ruber/static/media/RSymbol.b6f3e470.png"
+  },
+  {
+    "revision": "00bdc33d46a5a1b638f36f337eb99e63",
+    "url": "/ruber/static/media/edit-ride.00bdc33d.png"
+  }
+]);
